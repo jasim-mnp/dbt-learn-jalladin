27a762eb0749d4b91500c248b477a3920d00510f
jasim alladin <jasim.alladin@moonpig.com>
2020-05-13 17:03:15 +0000

    Organizing dbt

--- models/fct_orders.sql
+++
@@ -1,4 +0,0 @@
-Select * 
-from {{ ref('stg_orders') }} o
-    join {{ ref('stg_payments')} p
-    on o.order_id = p.order_id 
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,7 +1,6 @@
 Select 
     p.id as payment_id,
-    "orderID" as order_id
+    p."orderID" as order_id,
     p."paymentMethod" as payment_method,
     p.amount as amount
-from "RAW"."STRIPE"."PAYMENT"
-group by "orderID"
+from "RAW"."STRIPE"."PAYMENT" p
